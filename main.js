d23ad320997c02df55eae1543854717f3d826da1
Gustavo Barbosa <gbarbosagustavo01@gmail.com>
2023-02-28 13:48:26 -0300

    Correção do bug

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ $('form').on('submit', function(e){
     e.preventDefault()
 
     var tarefa = $('#endereco-tarefa').val();
-    var list = $('ul') 
-    var tarefasadd = $('#tarefasadd')
+    var list = $('<li></li>') 
 
     $(`<li> ${tarefa} </li>`).appendTo(list)
+
+    $(list).appendTo('ul')
 
     // Evento da tarefa feita
     $(list).on('click', function(){
